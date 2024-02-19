0d5d8b13debcc67e0ca760a3624874dbe3db3b68
Abdul Moiz <abdulmoiziphone4@gmail.com>
2024-02-19 12:55:59 +0500

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const urlRoute = require('./routes/url')
 const staticRoute = require('./routes/staticRouter')
 const URL = require('./models/url')
 const app  = express();
-const PORT =3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.set('view engine','ejs');
@@ -17,9 +16,9 @@ connectToMongoDB(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(error => console.error('Error connecting to MongoDB:', error));
 
-app.use('/url',urlRoute);
-
 app.use('/',staticRoute);
+
+app.use('/url',urlRoute);
 
 app.get('/url/:shortId', async (req,res) =>{
 
@@ -40,4 +39,12 @@ res.redirect(entry.redirectURL)
 });
 
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    try{
+        console.log(`The Server is Running on port:${PORT}`)
+    }
+    catch(e){
+        console.log(e)
+    }
+})
